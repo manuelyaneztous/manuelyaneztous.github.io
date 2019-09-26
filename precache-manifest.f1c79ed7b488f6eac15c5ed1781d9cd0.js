57454cdf26f9f356c1a6ffba5986cf2c1c596eb5
gatoloco-casa-port-lenovo-gl <gatoloco4916@gmail.com>
2019-09-26 17:01:03 +0200

    Updates

---
+++ precache-manifest.f1c79ed7b488f6eac15c5ed1781d9cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c2636f52eb20212d625419d5cd312e",
+    "url": "/manuelyaneztous.github.io/index.html"
+  },
+  {
+    "revision": "0c50fc91e59f8098cb92",
+    "url": "/manuelyaneztous.github.io/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "883b8dfb38a0c0ca2746",
+    "url": "/manuelyaneztous.github.io/static/js/2.3a3eacc4.chunk.js"
+  },
+  {
+    "revision": "0c50fc91e59f8098cb92",
+    "url": "/manuelyaneztous.github.io/static/js/main.cb7cafe0.chunk.js"
+  },
+  {
+    "revision": "719446da701976b1143e",
+    "url": "/manuelyaneztous.github.io/static/js/runtime-main.77f838b6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/manuelyaneztous.github.io/static/media/logo.25bf045c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/manuelyaneztous.github.io/precache-manifest.37bba38de72447d9976736e44643e7aa.js"
+  "/manuelyaneztous.github.io/precache-manifest.f1c79ed7b488f6eac15c5ed1781d9cd0.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/manuelyaneztous.github.io/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/manuelyaneztous.github.io/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
