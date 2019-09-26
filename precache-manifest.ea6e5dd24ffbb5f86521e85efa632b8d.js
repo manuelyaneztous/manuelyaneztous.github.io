2ab6308494eb87707b42a886176eb61238cfa85a
gatoloco-casa-port-lenovo-gl <gatoloco4916@gmail.com>
2019-09-26 15:55:33 +0200

    Updates

---
+++ precache-manifest.ea6e5dd24ffbb5f86521e85efa632b8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "ddfa04d8bdecd88a3361",
+    "url": "/me/static/js/main.ddfa04d8.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "ddfa04d8bdecd88a3361",
+    "url": "/me/static/css/main.b8f799fe.chunk.css"
+  },
+  {
+    "revision": "23a126030a800ea4866e053ebb5f8b19",
+    "url": "/me/index.html"
+  }
+];
