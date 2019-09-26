edb76bcafce0abca961177b681b588e5010e3a0d
gatoloco-casa-port-lenovo-gl <gatoloco4916@gmail.com>
2019-09-26 23:57:55 +0200

    Updates

---
+++ precache-manifest.5b241b2895f579597fabb4143647fc6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f92dc4f6b99791e8eca4",
+    "url": "/manuelyaneztous.github.io/static/js/runtime~main.f92dc4f6.js"
+  },
+  {
+    "revision": "8dc0726a479ad81635fc",
+    "url": "/manuelyaneztous.github.io/static/js/main.8dc0726a.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/manuelyaneztous.github.io/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "8dc0726a479ad81635fc",
+    "url": "/manuelyaneztous.github.io/static/css/main.beaa9fde.chunk.css"
+  },
+  {
+    "revision": "c9cfe531efd3016ff8b0505459bcd13d",
+    "url": "/manuelyaneztous.github.io/index.html"
+  }
+];
